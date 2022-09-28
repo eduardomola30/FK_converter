8761bc4d5051c96550fa6ac2bdfe13d4bfff84e6
eduardomola30 <114615222+eduardomola30@users.noreply.github.com>
2022-09-28 15:13:11 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        DecimalFormat fmt = new DecimalFormat("0.##");
+
+        System.out.println("Enter a fahrenheit degree ");
+        int f = input.nextInt();
+
+        // fahrenheit to kelvin
+        double k;
+        k = (f + 459.67) * 5/9;
+
+        System.out.println("The convertion to kelvin is: " + fmt.format(k));
+    }
+}
