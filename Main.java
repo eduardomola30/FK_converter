f9dac76bc3fe0771fea31658879b58ab4c4cf648
eduardomola30 <114615222+eduardomola30@users.noreply.github.com>
2022-09-28 15:15:06 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,7 +7,7 @@ public class Main {
         DecimalFormat fmt = new DecimalFormat("0.##");
 
         System.out.println("Enter a fahrenheit degree ");
-        int f = input.nextInt();
+        double f = input.nextInt();
 
         // fahrenheit to kelvin
         double k;
